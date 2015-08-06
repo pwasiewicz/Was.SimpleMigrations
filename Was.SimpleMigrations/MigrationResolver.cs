09cc3c23e735b68b4ab87d3316dfc7d6dc8aafd8
Patryk Wąsiewicz <patryk.wasiewicz@nau.pl>
2015-08-06 16:17:53 +0200

    Registering module fix

--- Was.SimpleMigrations/MigrationResolver.cs
+++ Was.SimpleMigrations/MigrationResolver.cs
@@ -1,15 +1,22 @@
 ﻿namespace Was.SimpleMigrations
 {
+    using System.Linq;
+    using System.Reflection;
     using Autofac;
     using System;
+    using EventBus.Autofac;
 
     public static class MigrationResolver
     {
-        public static void RunMigrations(Action<ContainerBuilder> dependenciesRegistrator)
+        public static void RunMigrations(Action<ContainerBuilder> dependenciesRegistrator, params Assembly[] assemblies)
         {
             var migrationBuilder = new ContainerBuilder();
 
             migrationBuilder.RegisterType<DataMigrationInterpreter>().AsSelf();
+
+            var totalAssemblies = assemblies.Concat(new[] { Assembly.GetAssembly(typeof(IDataMigration)) }).ToArray();
+            migrationBuilder.RegisterModule(new EventBusModule(totalAssemblies));
+
             dependenciesRegistrator(migrationBuilder);
 
             using (var scope = migrationBuilder.Build().BeginLifetimeScope())
